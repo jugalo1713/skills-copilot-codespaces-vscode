6005784f16becdfdade99a77532cabb7318a4536
Julian Gallo Londoño <64450166+jugalo1713@users.noreply.github.com>
2024-01-12 19:07:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,9 +1,50 @@
 // create web server
-const express = require('express');
-const app = express();
-const port = 3000;
-// connect to mongoDB
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/27017');
-// create mongoose schema
-const Comment = mongoose.model('Comment', {
+var express = require('express');
+var router = express.Router();
+// create database
+var mongoose = require('mongoose');
+// create model
+var Comment = require('../models/Comment.js');
+
+// GET comments listing
+router.get('/', function(req, res, next) {
+  Comment.find(function (err, comments) {
+    if (err) return next(err);
+    res.json(comments);
+  });
+});
+
+// POST /comments
+router.post('/', function(req, res, next) {
+  Comment.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// GET /comments/id
+router.get('/:id', function(req, res, next) {
+  Comment.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// PUT /comments/:id
+router.put('/:id', function(req, res, next) {
+  Comment.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// DELETE /comments/:id
+router.delete('/:id', function(req, res, next) {
+  Comment.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+// export router
+module.exports = router;
